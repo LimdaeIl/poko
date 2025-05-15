89aff0282e0c3be5dd63ac9c5b6eda3c72ae4970
LimdaeIl <optional94@naver.com>
2025-05-15 18:41:17 +0900

    🎉 프로젝트 초기화
    스프링부트 프로젝트를 초기화한다.

---
+++ src/test/java/com/poko/PokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
