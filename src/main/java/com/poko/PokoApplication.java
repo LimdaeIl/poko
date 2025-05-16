5e70a2f8301588deb03b3de34eab9ff8d1f52fce
LimdaeIl <optional94@naver.com>
2025-05-16 10:35:31 +0900

    🔧#7: Dotenv로 application.yml 파일에 .env 적용
    민감한 정보가 노출되지 않도록 .env 파일을 작성한다.
    스프링부트 클래스 파일에 Dotenv를 설정해서 .env 파일 경로에 따라 적용할 수 있도록 설정한다.
    
    참고로 도커 컴포즈는 동일한 디렉터리 내에 .env 파일이 있는데, 이것은 도커 컴포즈 전용 환경 설정 파일이라는 점을 참고하면 된다. 따라서 도커 컴포즈와 스프링 부트 애플리케이션의 서로 독립적인 .env 파일 설정을 한 것이다.

--- src/main/java/com/poko/PokoApplication.java
+++ src/main/java/com/poko/PokoApplication.java
@@ -1,12 +1,23 @@
 package com.poko;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PokoApplication {
 
+
 	public static void main(String[] args) {
+		// .env 파일 로드 (resources 기준 경로로)
+		Dotenv dotenv = Dotenv.configure()
+				.directory("src/main/resources") // 또는 .env 의 실제 경로
+				.ignoreIfMissing()
+				.load();
+
+		// 시스템 프로퍼티에 등록 (Spring 이 인식할 수 있도록)
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(PokoApplication.class, args);
 	}
 
