78ee30cc3588784c747fed7e4935e470f46c1184
LimdaeIl <optional94@naver.com>
2025-05-17 17:54:49 +0900

    ✨#12: 회원 엔티티 구현
    - SuperBuilder를 사용
    - 회원, 점주, 배송기사, 관리자 4명의 권한이 존재

---
+++ src/main/java/com/poko/apps/user/domain/entity/User.java
@@ -0,0 +1,51 @@
+package com.poko.apps.user.domain.entity;
+
+import com.poko.apps.common.domain.entity.BaseEntity;
+import com.poko.apps.user.domain.enums.user.UserRoleType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+@Table(name = "p_users")
+@Entity
+public class User extends BaseEntity {
+
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @Column(name = "id", nullable = false, unique = true)
+  private Long id;
+
+  @Column(name = "email", nullable = false, unique = true)
+  private String email;
+
+  @Column(name = "password", nullable = false)
+  private String password;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "phone", nullable = false, unique = true)
+  private String phone;
+
+  @Column(name = "profile_image")
+  private String profileImage;
+
+  @Builder.Default
+  @Enumerated(EnumType.STRING)
+  @Column(name = "role", nullable = false)
+  private UserRoleType userRoleType = UserRoleType.ROLE_USER;
+
+}
