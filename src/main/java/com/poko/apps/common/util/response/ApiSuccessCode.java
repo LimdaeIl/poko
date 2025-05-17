1f6a421c9dcc670987bfa83da9c444540fccbc35
LimdaeIl <optional94@naver.com>
2025-05-17 17:52:45 +0900

    ✨#12: 공통 성공 응답 및 실패 구현
    - 공통된 응답을 수행할 수 있도록 구현
    - `0`: 성공
    - `-1`: 실패

---
+++ src/main/java/com/poko/apps/common/util/response/ApiSuccessCode.java
@@ -0,0 +1,15 @@
+package com.poko.apps.common.util.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ApiSuccessCode implements SuccessCode {
+  OK(0, "OK", HttpStatus.OK);
+
+  private final Integer code;
+  private final String message;
+  private final HttpStatus httpStatus;
+}
