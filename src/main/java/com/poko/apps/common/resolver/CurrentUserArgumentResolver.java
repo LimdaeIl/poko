63a9b6dc6dd88802a4292d40f2a737e194b7bc9c
LimdaeIl <optional94@naver.com>
2025-05-19 17:29:03 +0900

    ✨#24:  리졸버 권한 체크 세분화

--- src/main/java/com/poko/apps/common/resolver/CurrentUserArgumentResolver.java
+++ src/main/java/com/poko/apps/common/resolver/CurrentUserArgumentResolver.java
@@ -1,11 +1,13 @@
 package com.poko.apps.common.resolver;
 
-import static com.poko.apps.common.exception.CommonErrorCode.INVALID_INPUT;
+import static com.poko.apps.common.exception.CommonErrorCode.INVALID_ROLE_VALUE;
+import static com.poko.apps.common.exception.CommonErrorCode.INVALID_USER_ID_VALUE;
 import static com.poko.apps.common.exception.CommonErrorCode.UNAUTHORIZED;
 
 import com.poko.apps.common.domain.enums.UserRoleType;
 import com.poko.apps.common.domain.vo.CurrentUserInfo;
 import com.poko.apps.common.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -13,6 +15,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @Component
 public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
 
@@ -38,12 +41,20 @@ public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolve
 
     try {
       long id = Long.parseLong(userId);
+
+      if (id <= 0) {
+        log.warn("유효하지 않은 userId 값: {}", userId);
+        throw new CustomException(INVALID_USER_ID_VALUE);
+      }
+
       UserRoleType role = UserRoleType.valueOf(userRole);
       return CurrentUserInfo.of(id, role);
     } catch (NumberFormatException e) {
-      throw new CustomException(INVALID_INPUT);
+      log.warn("유효하지 않은 userId 형식: {}", e.toString());
+      throw new CustomException(INVALID_USER_ID_VALUE);
     } catch (IllegalArgumentException e) {
-      throw new CustomException(UNAUTHORIZED);
+      log.warn("유효하지 않은 userRole 값: {}", userRole);
+      throw new CustomException(INVALID_ROLE_VALUE);
     }
   }
 }
