3297cc229a2f9fdc2695738751ea1b066713eb89
LimdaeIl <optional94@naver.com>
2025-05-21 15:51:01 +0900

    ✨#30: 회원 이메일 수정 기능 구현
    ADMIN 이면 다른 회원의 이메일을 수정할 수 있습니다. 그 외에 권한들은 자기 자신의 이메일만 수정할 수 있도록 분기 처리했습니다.

---
+++ src/main/java/com/poko/apps/user/application/dto/auth/response/PatchUserEmailResponse.java
@@ -0,0 +1,25 @@
+package com.poko.apps.user.application.dto.auth.response;
+
+import com.poko.apps.user.domain.entity.User;
+import lombok.Builder;
+
+public record PatchUserEmailResponse(
+    NewEmail newEmail
+) {
+
+  public static PatchUserEmailResponse from(User user) {
+    return new PatchUserEmailResponse(
+        NewEmail.builder()
+            .id(user.getId())
+            .email(user.getEmail())
+            .build()
+    );
+  }
+
+  @Builder
+  record NewEmail(
+      Long id,
+      String email
+  ) {
+  }
+}
