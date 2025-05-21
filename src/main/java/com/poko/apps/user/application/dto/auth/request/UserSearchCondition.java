22c7ece4805c934077d1c1cb9ee505405df707f8
LimdaeIl <optional94@naver.com>
2025-05-21 13:59:44 +0900

    ✨#28: 요청 파라미터를 condation 으로 관리
    `@ParameterObject`를 활용해서 모든 요청 파라미터를 받습니다.

---
+++ src/main/java/com/poko/apps/user/application/dto/auth/request/UserSearchCondition.java
@@ -0,0 +1,22 @@
+package com.poko.apps.user.application.dto.auth.request;
+
+import com.poko.apps.user.domain.enums.user.UserRoleType;
+import java.time.LocalDateTime;
+
+public record UserSearchCondition(
+    Long id,
+    String email,
+    String name,
+    String phone,
+    UserRoleType userRoleType,
+    Long createdBy,
+    LocalDateTime createdFrom,
+    LocalDateTime createdTo,
+    Long modifiedBy,
+    LocalDateTime modifiedFrom,
+    LocalDateTime modifiedTo,
+    Long deletedBy,
+    LocalDateTime deletedFrom,
+    LocalDateTime deletedTo
+) {
+}
