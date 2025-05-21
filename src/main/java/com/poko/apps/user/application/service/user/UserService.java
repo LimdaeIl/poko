f20c80270b25761f26e4c063adeeca83165d4605
LimdaeIl <optional94@naver.com>
2025-05-21 14:00:13 +0900

    ✨#28: 서비스 구현

--- src/main/java/com/poko/apps/user/application/service/user/UserService.java
+++ src/main/java/com/poko/apps/user/application/service/user/UserService.java
@@ -1,9 +1,15 @@
 package com.poko.apps.user.application.service.user;
 
 import com.poko.apps.common.domain.vo.CurrentUserInfo;
+import com.poko.apps.user.application.dto.auth.request.UserSearchCondition;
 import com.poko.apps.user.application.dto.auth.response.GetUserResponse;
+import com.poko.apps.user.application.dto.auth.response.GetUsersResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService {
 
   GetUserResponse getMyProfile(CurrentUserInfo info);
+
+  Page<GetUsersResponse> getUsers(UserSearchCondition condition, Pageable pageable);
 }
--- src/main/java/com/poko/apps/user/application/service/user/UserServiceImpl.java
+++ src/main/java/com/poko/apps/user/application/service/user/UserServiceImpl.java
@@ -8,10 +8,15 @@ import static com.poko.apps.user.domain.enums.user.UserErrorCode.USER_NOT_FOUND_
 
 import com.poko.apps.common.domain.vo.CurrentUserInfo;
 import com.poko.apps.common.exception.CustomException;
+import com.poko.apps.user.application.dto.auth.request.UserSearchCondition;
 import com.poko.apps.user.application.dto.auth.response.GetUserResponse;
+import com.poko.apps.user.application.dto.auth.response.GetUsersResponse;
 import com.poko.apps.user.domain.entity.User;
+import com.poko.apps.user.domain.repository.user.UserQueryRepository;
 import com.poko.apps.user.domain.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
+  private final UserQueryRepository userQueryRepository;
 
   private User findUserById(Long userId) {
     return userRepository.findById(userId)
@@ -42,6 +48,11 @@ public class UserServiceImpl implements UserService {
     return GetUserResponse.from(userById);
   }
 
-  
+  @Override
+  public Page<GetUsersResponse> getUsers(UserSearchCondition condition, Pageable pageable) {
+
+    return userQueryRepository.getUsersByCondition(condition, pageable);
+  }
+
 
 }
