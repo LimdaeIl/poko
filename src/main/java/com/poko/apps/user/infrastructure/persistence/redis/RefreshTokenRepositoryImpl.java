ec0aa5815a2a34bbc8182c1625bb76ff64308c35
LimdaeIl <optional94@naver.com>
2025-05-22 15:22:52 +0900

    ✨#36: 이메일 저장 및 조회 구현
    이메일을 레디스에 저장하고 조회할 수 있는 기능을 구현했습니다.

--- src/main/java/com/poko/apps/user/infrastructure/persistence/redis/RefreshTokenRepositoryImpl.java
+++ src/main/java/com/poko/apps/user/infrastructure/persistence/redis/RefreshTokenRepositoryImpl.java
@@ -15,6 +15,19 @@ public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
   private static final String LOGOUT = "LOGOUT";
 
   @Override
+  public String getEmailCode(String email) {
+    String key = "EC:" + email;
+    return redisTemplate.opsForValue().get(key);
+
+  }
+
+  @Override
+  public void setEmailCode(String email, String code, Duration duration) {
+    String key = "EC:" + email;
+    redisTemplate.opsForValue().set(key, code, duration);
+  }
+
+  @Override
   public void saveRefreshToken(Long userId, String refreshToken, long ttlMillis) {
     String key = "RT:" + userId;
     redisTemplate.opsForValue().set(key, refreshToken, Duration.ofMillis(ttlMillis)); // TODO: 트러블슈팅 TTL 작성
