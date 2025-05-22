3b1fd68a2d18ff188627eddbd9408f99dd5d8059
LimdaeIl <optional94@naver.com>
2025-05-22 15:23:15 +0900

    ✨#36: 이메일 인증 및 검증 컨트롤러 구현

--- src/main/java/com/poko/apps/user/domain/enums/user/UserSuccessCode.java
+++ src/main/java/com/poko/apps/user/domain/enums/user/UserSuccessCode.java
@@ -15,7 +15,9 @@ public enum UserSuccessCode implements SuccessCode {
   USERS_GET_SUCCESS(0, "회원 목록 조회에 성공했습니다.", OK),
   USER_DELETE_BY_ID_SUCCESS(0, "회원 삭제에 성공했습니다.", OK),
   USER_PATCH_ROLE_SUCCESS(0, "회원 권한 수정에 성공했습니다.", OK),
-  USER_PATCH_PASSWORD_SUCCESS(0, "회원 비밀번호 수정에 성공했습니다.", OK);
+  USER_PATCH_PASSWORD_SUCCESS(0, "회원 비밀번호 수정에 성공했습니다.", OK),
+  SEND_CODE_SUCCESS(0, "이메일 인증 코드 전송에 성공했습니다.", OK),
+  VERIFY_CODE_SUCCESS(0, "이메일 인증 코드가 동일합니다.", OK);
 
   private final Integer code;
   private final String message;
