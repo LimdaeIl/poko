5c486651f5f9742d6825ddd12161f4e7de05d48e
LimdaeIl <optional94@naver.com>
2025-05-22 15:21:54 +0900

    ✨#36: 이메일 인증 코드 전송 및 검증 구현
    레디스에 보관되는 시간을 3분으로 고정했습니다.
    
    하드 코딩으로 이메일에 전송되는데, 고도화에서는 템플릿으로 제공할 예정입니다.

--- src/main/java/com/poko/apps/user/application/service/auth/AuthService.java
+++ src/main/java/com/poko/apps/user/application/service/auth/AuthService.java
@@ -1,13 +1,16 @@
 package com.poko.apps.user.application.service.auth;
 
+import com.poko.apps.user.application.dto.auth.request.EmailVerifyRequest;
 import com.poko.apps.user.application.dto.auth.request.ExistsEmailRequest;
 import com.poko.apps.user.application.dto.auth.request.ExistsPhoneRequest;
 import com.poko.apps.user.application.dto.auth.request.GenerateTokenRequest;
 import com.poko.apps.user.application.dto.auth.request.LoginRequest;
 import com.poko.apps.user.application.dto.auth.request.SignupRequest;
+import com.poko.apps.user.application.dto.auth.response.EmailCodeResponse;
 import com.poko.apps.user.application.dto.auth.response.GenerateTokenResponse;
 import com.poko.apps.user.application.dto.auth.response.LoginResponse;
 import com.poko.apps.user.application.dto.auth.response.SignupResponse;
+import com.poko.apps.user.application.dto.auth.request.EmailCodeRequest;
 import com.poko.apps.user.domain.enums.auth.AuthSuccessCode;
 import jakarta.validation.Valid;
 
@@ -23,4 +26,8 @@ public interface AuthService {
   void logout(String accessToken);
 
   GenerateTokenResponse generateToken(String accessToken, GenerateTokenRequest request);
+
+  EmailCodeResponse emailCode(EmailCodeRequest request);
+
+  void verifyCode(EmailVerifyRequest request);
 }
--- src/main/java/com/poko/apps/user/application/service/auth/AuthServiceImpl.java
+++ src/main/java/com/poko/apps/user/application/service/auth/AuthServiceImpl.java
@@ -1,24 +1,36 @@
 package com.poko.apps.user.application.service.auth;
 
 import static com.poko.apps.common.exception.CommonErrorCode.INVALID_INPUT;
-import static com.poko.apps.user.domain.enums.auth.AuthErrorCode.*;
+import static com.poko.apps.user.domain.enums.auth.AuthErrorCode.INVALID_BEARER_TOKEN;
+import static com.poko.apps.user.domain.enums.auth.AuthErrorCode.INVALID_PASSWORD;
+import static com.poko.apps.user.domain.enums.auth.AuthErrorCode.USER_NOT_FOUND_BY_EMAIL;
+import static com.poko.apps.user.domain.enums.auth.AuthErrorCode.USER_NOT_FOUND_BY_ID;
+import static com.poko.apps.user.domain.enums.auth.AuthErrorCode.USER_NOT_FOUND_BY_PHONE;
 
 import com.poko.apps.common.exception.CustomException;
+import com.poko.apps.user.application.dto.auth.request.EmailVerifyRequest;
 import com.poko.apps.user.application.dto.auth.request.ExistsEmailRequest;
 import com.poko.apps.user.application.dto.auth.request.ExistsPhoneRequest;
 import com.poko.apps.user.application.dto.auth.request.GenerateTokenRequest;
 import com.poko.apps.user.application.dto.auth.request.LoginRequest;
 import com.poko.apps.user.application.dto.auth.request.SignupRequest;
+import com.poko.apps.user.application.dto.auth.response.EmailCodeResponse;
 import com.poko.apps.user.application.dto.auth.response.GenerateTokenResponse;
 import com.poko.apps.user.application.dto.auth.response.LoginResponse;
 import com.poko.apps.user.application.dto.auth.response.SignupResponse;
+import com.poko.apps.user.application.dto.auth.request.EmailCodeRequest;
 import com.poko.apps.user.domain.entity.User;
 import com.poko.apps.user.domain.enums.auth.AuthSuccessCode;
 import com.poko.apps.user.domain.jwt.JwtProvider;
 import com.poko.apps.user.domain.repository.auth.AuthRepository;
 import com.poko.apps.user.domain.repository.auth.RefreshTokenRepository;
+import java.time.Duration;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +44,10 @@ public class AuthServiceImpl implements AuthService {
   private final PasswordEncoder passwordEncoder;
   private final JwtProvider jwtProvider;
   private final RefreshTokenRepository refreshTokenRepository;
+  private final JavaMailSender mailSender;
+
+  @Value("${spring.mail.username}")
+  private String email;
 
 
   private boolean existsUserByEmail(String email) {
@@ -145,4 +161,50 @@ public class AuthServiceImpl implements AuthService {
 
     return GenerateTokenResponse.of(newAccessToken, newRefreshToken);
   }
+
+  @Override
+  public EmailCodeResponse emailCode(EmailCodeRequest request) {
+    Integer code = generateRandomNumber();
+
+    // Redis 등에 저장 (TTL: 예 3분)
+    refreshTokenRepository.setEmailCode(request.email(), code.toString(), Duration.ofMinutes(3));
+
+    // 이메일 전송
+    send(request.email(), code);
+
+    return new EmailCodeResponse("인증 코드가 전송되었습니다.");
+  }
+
+  @Override
+  public void verifyCode(EmailVerifyRequest request) {
+    String savedCode = refreshTokenRepository.getEmailCode(request.email());
+    String inputCode = request.verifyCode().toString();
+
+    if (savedCode.isBlank() || inputCode.isBlank() || !savedCode.equals(inputCode)) {
+      throw new CustomException(INVALID_INPUT);
+    }
+
+  }
+
+
+  public void send(String to, int code) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setTo(to);
+    message.setFrom(email);
+    message.setSubject("[POKO] 이메일 인증 코드");
+    message.setText("인증 코드: " + code + "\n3분 이내에 입력해 주세요.");
+    mailSender.send(message);
+  }
+
+  private Integer generateRandomNumber() {
+    Random random = new Random();
+    StringBuilder randomNumber = new StringBuilder();
+    for (int i = 0; i < 6; i++) {
+      randomNumber.append(random.nextInt(10));
+    }
+    return Integer.parseInt(randomNumber.toString());
+  }
+
+
+
 }
